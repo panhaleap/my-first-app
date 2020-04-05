faf155f4184370227db94a552e52685098f9ee72
panhaleap <leappanha1@yahoo.com>
2020-04-05 20:55:58 +0700

    ngFor with index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent {
   log = [];
   onTooggleDisplay() {
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date);
   }
 }
