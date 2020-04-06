078e4745dbeb305e1b9919ffd7e15c9c4e1ef142
panhaleap <panhaleap@gmail.com>
2020-04-06 11:13:03 +0700

    delete previous practise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-    h3 {
-      color: dodgerblue;
-    }`]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  username;
-  showSecret = false;
-  log = [];
-  onTooggleDisplay() {
-    this.showSecret = !this.showSecret;
-    // this.log.push(this.log.length + 1);
-    this.log.push(new Date);
-  }
 }
