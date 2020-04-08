53bcef96b830b832cc02287bbc5f90f422d18d72
panhaleap <panhaleap@gmail.com>
2020-04-08 13:21:15 +0700

    ssss

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,6 +4,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
+// tslint:disable-next-line: component-class-suffix
 export class HeaderComponet {
     @Output() featureSelected = new EventEmitter<string>();
 
